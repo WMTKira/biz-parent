a66668cd0ba2878328410237956f5f199bd6365d
wmtumanday <wiljohnyumanday@gmail.com>
2024-09-17 15:56:16 +0800

    parent procject

---
+++ api-gateway/src/main/java/com/wmt/gateway/ApiGateway.java
@@ -0,0 +1,14 @@
+package com.wmt.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wmtumanday
+ */
+@SpringBootApplication
+public class ApiGateway {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGateway.class, args);
+    }
+}
---
+++ config-server/src/main/java/com/wmt/configserver/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.wmt.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author wmtumanday
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
